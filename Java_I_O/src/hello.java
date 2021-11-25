d836c9f8c0fc863f531a0c4545382bbca73e5678
Fabiojpl1907 <fabiojpl.1907@gmail.com>
2021-11-25 16:07:37 -0300

    Exercícios de Java IO

---
+++ Java_I_O/src/hello.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+
+public class hello {
+
+        public static void receberTecladoImprimirConsole() throws IOException {
+
+        /*InputStream is = System.in;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);*/
+            System.out.println("Recomende 3 filmes: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line = br.readLine();
+
+        /*OutputStream os = System.out;
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);*/
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            do {
+                bw.write(line);
+                bw.newLine();
+                line = br.readLine();
+            } while(!(line.isEmpty()));
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+        }
+
+        public static void main(String[] args) throws IOException {
+            receberTecladoImprimirConsole();
+        }
+
+
+}
